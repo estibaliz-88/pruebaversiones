43e3e60f613c170e2ebafd889f3dc97e4125e729
estibaliz-88 <admin@10.10.0.149>
2015-05-19 10:23:15 +0300

    Modificando

---
+++ Pruebagithub/src/Principal.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Probando versiones");
+
+	}
+
+}
